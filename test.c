688ed0b9d112b2fcf1e1932505f556e1b4ce2c61
Moritz Hoffmann <moritz.hoffmann@tutanota.com>
2025-08-17 15:51:30 +0100

    removal of printfs

--- test.c
+++ test.c
@@ -2,8 +2,6 @@
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
-   printf("Guten Morgen!");
-   printf("Hello again!");
    int i = 3;
    printf("%d", i);
    return 0;
