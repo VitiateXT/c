f41a750a1b5c18dfbd701ec6f01df8ed4cb4874f
Moritz Hoffmann <moritz.hoffmann@tutanota.com>
2025-08-17 15:54:00 +0100

    added variables

--- test.c
+++ test.c
@@ -3,6 +3,7 @@ int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
    int i = 3;
+   int a = 5;
    printf("Printing variable:");
    printf("%d", i);
    return 0;
